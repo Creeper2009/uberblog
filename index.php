5753512d153a90a995f7d9c436be58042fcebdf0
UberChampion <ryanbeltzbackup@gmail.com>
2014-09-22 16:48:13 -0400

    Accidentally committed
    Change create.php to whatever you want.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Index</title>
+		<link type="text/css" rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+			<div id="wrapper">
+				<div id="header">
+					<h1>Example Uberblog</h1>
+					<p>Sample Text</p>
+				</div>
+				<div id="content">
+					
+					<div class="News">
+					    <marquee><h3>News</h3></marquee>
+					</div>
+					<?php include('posts.php'); ?>
+				</div>
+			</div>
+	</body>
+</html>
